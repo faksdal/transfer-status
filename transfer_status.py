811fe3765d523450d66900d81659c8beb42c2794
Jon Leithe <jon@jonleithe.no>
2025-08-24 18:15:09 +0200

    Returns active transfers for ns and nn

--- transfer_status.py
+++ transfer_status.py
@@ -137,7 +137,8 @@ def _print_matrix(headers: Sequence[str], rows: Sequence[Sequence[str]]) -> None
 
     # Data
     for r in rows:
-        if r[1].find(self, "nn")
+        # cell = r[1].get_text(strip=True)
+        if("ns" in r[1].lower() or "nn" in r[1].lower()):
             print(fmt_row(r))
 
 
